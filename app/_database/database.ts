9635abc6799977bf0518e5d447438b26b83da45b
Mohammad Ismail <mohammadn.ismail@mail.utoronto.ca>
2023-12-05 01:36:52 -0500

    temp: change cert to scram for lab submission

--- app/_database/database.ts
+++ app/_database/database.ts
@@ -5,12 +5,16 @@ export const certFile = '/tmp/nextcrypt-cert-X509.pem';
 export async function connectDB() {
   try {
     mongoose.connect(process.env.MONGODB_URI!, {
-      ssl: true,
-      tlsCertificateKeyFile: certFile,
-      authMechanism: "MONGODB-X509",
-      authSource: "$external",
       dbName: process.env.MONGODB_DATABASE!
     })
+    // TODO: revert to cert after lab (vercel deployment issues w/ cert)
+    // mongoose.connect(process.env.MONGODB_URI!, {
+    //   ssl: true,
+    //   tlsCertificateKeyFile: certFile,
+    //   authMechanism: "MONGODB-X509",
+    //   authSource: "$external",
+    //   dbName: process.env.MONGODB_DATABASE!
+    // })
     const connection = mongoose.connection;
     connection.on('connected', () => {
       console.log("connected to db");
